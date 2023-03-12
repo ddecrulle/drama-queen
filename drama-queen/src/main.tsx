0ac7a764c4dfb0bba7d91bfa0e9269893dfc1db5
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-03-12 20:23:24 +0100

    static import

--- drama-queen/src/main.tsx
+++ drama-queen/src/main.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
@@ -7,8 +7,8 @@ import { router } from './routes/root'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
-      <RouterProvider router={router} />
-    </Suspense>
+    {/* <Suspense fallback={<div>Loading...</div>}> */}
+    <RouterProvider router={router} />
+    {/* </Suspense> */}
   </React.StrictMode>,
 )
