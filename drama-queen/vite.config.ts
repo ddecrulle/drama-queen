fc5c43f4c967975bfa144a7997cb3d1df098136f
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-03-12 22:48:27 +0100

    Update vite.config.ts

--- drama-queen/vite.config.ts
+++ drama-queen/vite.config.ts
@@ -6,20 +6,21 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
+  const getEnvVar = (key: string) => {
+    if (typeof window !== "undefined") {
+      return env[key] || (!window as any)?._env_?.[key];
+    }
+    return env[key];
+  };
   return {
     plugins: [
       react(),
       federation({
         name: "drama-queen",
         remotes: {
-          queen:
-            env.VITE_QUEEN_URL ||
-            (window as any)?._env_?.["VITE_QUEEN_URL"] +
-              "/assets/remoteEntryQueen.js",
+          queen: getEnvVar("VITE_QUEEN_URL") + "/assets/remoteEntryQueen.js",
           queen_v2:
-            env.VITE_QUEEN_V2_URL ||
-            (!window as any)?._env_?.["VITE_QUEEN_V2_URL"] +
-              "/assets/remoteEntryQueenV2.js",
+            getEnvVar("VITE_QUEEN_V2_URL") + +"/assets/remoteEntryQueenV2.js",
         },
         shared: ["react", "react-dom"],
       }),
