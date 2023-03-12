74a95eb39ca9e98a6d18297f9722179100e32eaf
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-03-12 19:41:29 +0100

    dynamic import

--- drama-queen/src/routes/root.tsx
+++ drama-queen/src/routes/root.tsx
@@ -2,9 +2,12 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 import DramaQueen from "../App";
+import React from "react";
+//import QueenApp from "queen/App";
+//import App from "queen_v2/App";
 
-import QueenApp from "queen/App";
-import App from "queen_v2/App";
+const QueenApp = React.lazy(() => import('queen/App'))
+const App = React.lazy(() => import('queen_v2/App'))
 
 export const router = createBrowserRouter([
   {
