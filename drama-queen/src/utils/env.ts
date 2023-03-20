86f684b334f71dc2673e9f5dd361568f1ffaaf2b
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-03-20 09:05:40 +0100

    PWA : Make PWA with Queen Service Worker (#2)
    * pwa-test
    
    * first step
    
    * Update env.ts
    
    * Update sw-pearl.js
    
    * inject queen sw
    
    * pwa works but env build time not runtime

--- drama-queen/src/utils/env.ts
+++ drama-queen/src/utils/env.ts
@@ -1,2 +1,3 @@
-export const getEnvVar = (key: string) =>
-(window as any)?._env_?.[key] || import.meta.env[key];
+export const getEnvVar = <K extends keyof ImportMetaEnv>(
+  key: K
+): ImportMetaEnv[K] => (window as any)?._env_?.[key] ?? import.meta.env[key];
