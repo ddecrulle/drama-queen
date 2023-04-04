a7a09f025e6c0ae44c5dba02de4322c3d2966a26
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-04-04 17:03:18 +0200

    Update IntegratedMapping.tsx

--- drama-queen/src/pages/QueenMapping/IntegratedMapping.tsx
+++ drama-queen/src/pages/QueenMapping/IntegratedMapping.tsx
@@ -3,7 +3,8 @@ import { useParams } from 'react-router-dom'
 
 function IntegratedMapping() {
   const { campaignId } = useParams()
-  return campaignId && import.meta.env.VITE_CAMPAIGN_ID_V2.includes(campaignId) ? <queen-v2-app /> : <queen-app />
+  
+  return campaignId?.toLowerCase().includes("v2") ? <queen-v2-app /> : <queen-app />
 }
 
 export default IntegratedMapping
