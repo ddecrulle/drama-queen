f82a87577657de030c8296906033ebf0e1323cb5
Dylan Decrulle <dylan.decrulle@outlook.fr>
2023-04-07 11:53:31 +0200

    remove unused import and echo

--- drama-queen/src/App.tsx
+++ drama-queen/src/App.tsx
@@ -1,7 +1,6 @@
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from 'routes/root'
-import { getEnvVar } from 'utils/env'
 
 function App() {
   return (
